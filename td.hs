07d745651e069eb42f30bcb936a9fb27f6097837
Ettore Forigo <ettore.forigo@gmail.com>
2021-12-02 00:37:46 +0100

    upload

---
+++ td.hs
@@ -0,0 +1,46 @@
+import Control.Category ((>>>))
+import Control.Monad (unless)
+import Data.Set (Set, empty, insert, delete, toList)
+import System.IO (stdout, hFlush)
+
+type Item = String
+type State = Set Item
+
+initial :: State
+initial = empty
+
+add :: Item -> State -> State
+add = insert
+
+remove :: Int -> State -> State
+remove i = flip delete <*> (toList >>> (!! i))
+
+edit :: Int -> Item -> State -> State
+edit i t = (remove i) >>> (add t)
+
+exec :: [String] -> State -> State
+exec ("a":ss) = add $ unwords ss
+exec ("r":i:[]) = remove (read i)
+exec ("e":i:ss) = edit (read i) $ unwords ss
+exec _ = id
+
+format :: (Int, Item) -> String
+format (i, s) = (show i) ++ " " ++ s
+
+menu :: State -> IO ()
+menu s = do
+  putStrLn ""
+  putStrLn "List:"
+  mapM_ (format >>> putStrLn) $ zip [0..] $ toList s
+  putStrLn "--- ~ ---"
+  putStrLn ""
+
+  putStr "> "
+  hFlush stdout
+  cmd <- getLine
+
+  unless (cmd == "q")
+    $ menu $ exec (words cmd) s
+
+main :: IO ()
+main = menu initial
---
+++ tdi.hs
@@ -0,0 +1,89 @@
+import Control.Category ((>>>))
+import Control.Monad (unless, void)
+import Data.Function ((&))
+import Data.List (isInfixOf)
+import Data.Set (Set, empty, toList, insert, delete)
+import System.IO (stdout, hFlush)
+import System.Process (system)
+
+type Item = String
+data Mode = C | A | R | E deriving Eq
+type Text = String
+data State = State (Set Item) Mode Text
+
+instance Show Mode where
+  show C = "!"
+  show A = "a"
+  show R = "r"
+  show E = "e"
+
+initial :: State
+initial = State empty C ""
+
+format :: (Int, Item) -> String
+format (i, s) = (show i) ++ " " ++ s
+
+setMode :: Mode -> State -> State
+setMode m (State s _ text) = State s m text
+
+clear :: State -> State
+clear (State set mode _) = State set mode ""
+
+add :: State -> State
+add (State set mode text) = State (insert text set) mode text
+
+remove :: State -> State
+remove (State set mode textIndex) = State (delete ((toList set) !! (read textIndex)) set) mode textIndex
+
+edit :: State -> State
+edit (State set mode textIndex) = let item = (toList set) !! (read textIndex) in State (delete item set) mode item
+
+textbox :: (State -> State) -> Char -> State -> State
+textbox _      '!'    s                     = ((setMode C) . clear) s
+textbox action '\n'   s                     = action s
+textbox _      '\DEL' (State set mode "")   = State set mode ""
+textbox _      '\DEL' (State set mode text) = State set mode (init text)
+textbox _      char   (State set mode text) = State set mode (text ++ char:[])
+
+cmdbox :: Char -> State -> State
+cmdbox 'a' = setMode A
+cmdbox 'r' = setMode R
+cmdbox 'e' = setMode E
+cmdbox _   = id
+
+modebox :: State -> Char -> State -> State
+modebox (State _ C _) = cmdbox
+modebox (State _ A _) = textbox (   add >>> clear >>> (setMode C))
+modebox (State _ R _) = textbox (remove >>> clear >>> (setMode C))
+modebox (State _ E _) = textbox (  edit           >>> (setMode A))
+
+menu :: State -> IO ()
+menu s = do
+  system "clear"
+
+  putStrLn "List:"
+  let State set mode text = s in
+    (toList set)
+    & (
+      if (mode == A)
+      then filter (isInfixOf text)
+      else id
+    )
+    & zip [0..]
+    & mapM_ (format >>> putStrLn)
+  putStrLn "--- ~ ---"
+  putStrLn ""
+
+  let State _ mode text = s in
+    putStr $ (show mode) ++ " > " ++ text
+  hFlush stdout
+  char <- getChar
+
+  unless (char == 'q')
+    $ menu
+      $ (modebox s) char s
+
+main :: IO ()
+main = do
+    menu initial
+    void $ system "clear"
